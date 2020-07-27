357215f56304c6bea885c89d4b2150714ac8d6ba
osboxes.org <guymdnicholls@yahoo.co.uk>
2020-07-27 13:34:14 +0100

    Added setup-bucket

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -11,6 +11,7 @@
 
 import boto3
 import click
+from botocore.exceptions import ClientError
 
 session=boto3.Session(profile_name='pythonAutomation')
 s3=session.resource('s3')
@@ -25,6 +26,7 @@ def list_buckets():
     "Lists all s3 buckets"
     for bucket in s3.buckets.all():
         print(bucket)
+#        print(session.region_name)
 
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
@@ -32,6 +34,55 @@ def list_bucket_objects(bucket):
     "Lists objects in an s3 bucket"
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and configure S3 bucket"
+    s3_bucket = None
+    try:
+        s3_bucket = s3.create_bucket(
+            Bucket=bucket
+#            Bucket=bucket,
+#            CreateBucketConfiguration={'LocationConstraint': session.region_name}
+#            CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'}
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket = s3.Bucket(bucket)
+        else:
+            raise e
+
+    policy = """
+    {
+      "Version":"2012-10-17",
+      "Statement":[{
+      "Sid":"PublicReadGetObject",
+      "Effect":"Allow",
+      "Principal": "*",
+          "Action":["s3:GetObject"],
+          "Resource":["arn:aws:s3:::%s/*"
+          ]
+        }
+      ]
+    }
+    """ % s3_bucket.name
+    policy = policy.strip()
+
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration={
+        'ErrorDocument': {
+            'Key': 'error.html'
+        },
+        'IndexDocument': {
+            'Suffix': 'index.html'
+        }
+    })
+
+    return
 
 if __name__ == '__main__':
     cli()
--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 
 - list buckets
 - lists contents of buckets
+- setup bucket
 
